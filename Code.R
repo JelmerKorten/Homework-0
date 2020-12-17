ce44e889c4c46265eb37e133c2bb76031bfb1518
JelmerKorten <jelmer.korten@gmail.com>
2020-12-17 12:38:06 +0100

    Commiting new script.

---
+++ Code.R
@@ -0,0 +1,14 @@
+library(tidyverse)
+#pull 
+#from remote Repo, GitHub
+
+#add
+
+#commit
+#push
+
+#yellow square = Git knows nothing
+
+#Add file,
+#commit chance to local repo
+#push towards github repo
