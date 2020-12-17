4e609e6b74599a13d33bff0b7b1b67c0fe54bdb1
JelmerKorten <jelmer.korten@gmail.com>
2020-12-17 12:58:40 +0100

    Testing auto login

--- Code.R
+++ Code.R
@@ -1,14 +1,2 @@
 library(tidyverse)
-#pull 
-#from remote Repo, GitHub
-
-#add
-
-#commit
-#push
-
-#yellow square = Git knows nothing
-
-#Add file,
-#commit chance to local repo
-#push towards github repo
+test
